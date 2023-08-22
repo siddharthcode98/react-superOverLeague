5ee12fd0d84eceed963bf7cbd6b86a5540bac73e
siddharthcode98 <vaddemanisiddharth@gmail.com>
2023-08-22 16:42:21 +0000

    committed code

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div class="bg-container">
+    <h1 class="heading">Super Over League</h1>
+    <div class="image-container">
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      ></img>
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
